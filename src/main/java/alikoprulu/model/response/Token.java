5ba749c0da89373a7eadbb8c696f2c1a23ec5583
AliKOPRULU <aliperk@hotmail.com>
2016-11-30 11:53:37 +0300

    Client and token add

---
+++ src/main/java/alikoprulu/model/response/Token.java
@@ -0,0 +1,17 @@
+package alikoprulu.model.response;
+
+/**
+ * Created by Ali on 30.11.2016.
+ */
+public class Token {//Token is valid for 10 minutes.
+
+    private String token;
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+}
